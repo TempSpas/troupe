2dd8c31863326ac348ec89a96ebd8f6b57854e45
Lamarama <j.f.lamas.lopez@gmail.com>
2016-06-21 13:34:26 -0400

    ye

--- popup.js
+++ popup.js
@@ -56,7 +56,7 @@ for(var i = 0; i < elements.length; i++)
 
 		if(node.nodeType === 3) {
 			var text = node.nodeValue;
-			var replacedText = text.replace(/Trump/gi, 'Jared');
+			var replacedText = text.replace('Trump', 'Jared');
 
 			if(replacedText !== text) {
 				element.replaceChild(document.createTextNode(replacedText), node);
