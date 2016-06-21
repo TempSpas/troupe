7dd166c5750c9f7dc7787a0f381bba08ea8ec6ef
TempSpas <nicoverbeek@Belafonte.local>
2016-06-21 13:36:04 -0400

    Get rid of unnecessary Github code

--- popup.js
+++ popup.js
@@ -56,11 +56,7 @@ for(var i = 0; i < elements.length; i++)
 
 		if(node.nodeType === 3) {
 			var text = node.nodeValue;
-<<<<<<< HEAD
-			var replacedText = text.replace('Trump', 'Jared');
-=======
 			var replacedText = text.replace('Trump', 'Dong');
->>>>>>> origin/master
 
 			if(replacedText !== text) {
 				element.replaceChild(document.createTextNode(replacedText), node);
