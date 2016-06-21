ca135036d6ad3f68c0f2e18b8e3035618eef17cb
TempSpas <nicoverbeek@Belafonte.local>
2016-06-21 13:10:45 -0400

    Change button, possibly fix it

--- popup.js
+++ popup.js
@@ -1,46 +1,66 @@
 /* "Borrowed from the bookmarklet on the GTmetrix website" */
 
-// chrome.tabs.getSelected(null, function(tab)
-// {
-// 	var checkPageButton = document.getElementById('checkPage');
-// 	checkPageButton.addEventListener('click', function()
-// 	{
-// 		chrome.tabs.getSelected(null, function(tab)
-// 		{
-// 			d = document;
-
-// 			var f = d.createElement('form');
-// 			f.action = 'http://gtmetrix.com/analyze.html?bm';
-// 			f.method = 'post';
-
-// 			var i = d.createElement('input');
-// 			i.type = 'hidden';
-// 			i.name = 'url';
-// 			i.value = tab.url;
-// 			f.appendChild(i);
-// 			d.body.appendChild(f);
-// 			f.submit();
-// 		});
-// 	}, false);
-// }, false);
-
-var elements = document.getElementsByTagName('*');
-
-for(var i = 0; i < elements.length; i++)
+chrome.tabs.getSelected(null, function(tab)
 {
-	var element = elements[i];
-
-	for(var j = 0; j < element.childNodes.length; j++)
+	var checkPageButton = document.getElementById('checkPage');
+	checkPageButton.addEventListener('click', function()
 	{
-		var node = element.childNodes[j];
+		chrome.tabs.getSelected(null, function(tab)
+		{
+			var elements = document.getElementsByTagName('*');
 
-		if(node.nodeType === 3) {
-			var text = node.nodeValue;
-			var replacedText = text.replace(/Trump/gi, 'Jared');
+			for(var i = 0; i < elements.length; i++)
+			{
+				var element = elements[i];
 
-			if(replacedText !== text) {
-				element.replaceChild(document.createTextNode(replacedText), node);
+				for(var j = 0; j < element.childNodes.length; j++)
+				{
+					var node = element.childNodes[j];
+
+					if(node.nodeType === 3) {
+						var text = node.nodeValue;
+						var replacedText = text.replace(/Trump/gi, 'Jared');
+
+						if(replacedText !== text) {
+							element.replaceChild(document.createTextNode(replacedText), node);
+						}
+					}
+				}
 			}
-		}
-	}
-}
+			// d = document;
+
+			// var f = d.createElement('form');
+			// f.action = 'http://gtmetrix.com/analyze.html?bm';
+			// f.method = 'post';
+
+			// var i = d.createElement('input');
+			// i.type = 'hidden';
+			// i.name = 'url';
+			// i.value = tab.url;
+			// f.appendChild(i);
+			// d.body.appendChild(f);
+			// f.submit();
+		});
+	}, false);
+}, false);
+
+// var elements = document.getElementsByTagName('*');
+
+// for(var i = 0; i < elements.length; i++)
+// {
+// 	var element = elements[i];
+
+// 	for(var j = 0; j < element.childNodes.length; j++)
+// 	{
+// 		var node = element.childNodes[j];
+
+// 		if(node.nodeType === 3) {
+// 			var text = node.nodeValue;
+// 			var replacedText = text.replace(/Trump/gi, 'Jared');
+
+// 			if(replacedText !== text) {
+// 				element.replaceChild(document.createTextNode(replacedText), node);
+// 			}
+// 		}
+// 	}
+// }
