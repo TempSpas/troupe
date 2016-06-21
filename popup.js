869452e169f82bef7ca64bbfceeebd90d715aca4
TempSpas <nicoverbeek@Belafonte.local>
2016-06-21 13:03:01 -0400

    Add code to change Trump to Jared

--- popup.js
+++ popup.js
@@ -1,25 +1,46 @@
 /* "Borrowed from the bookmarklet on the GTmetrix website" */
 
-chrome.tabs.getSelected(null, function(tab)
+// chrome.tabs.getSelected(null, function(tab)
+// {
+// 	var checkPageButton = document.getElementById('checkPage');
+// 	checkPageButton.addEventListener('click', function()
+// 	{
+// 		chrome.tabs.getSelected(null, function(tab)
+// 		{
+// 			d = document;
+
+// 			var f = d.createElement('form');
+// 			f.action = 'http://gtmetrix.com/analyze.html?bm';
+// 			f.method = 'post';
+
+// 			var i = d.createElement('input');
+// 			i.type = 'hidden';
+// 			i.name = 'url';
+// 			i.value = tab.url;
+// 			f.appendChild(i);
+// 			d.body.appendChild(f);
+// 			f.submit();
+// 		});
+// 	}, false);
+// }, false);
+
+var elements = document.getElementsByTagName('*');
+
+for(var i = 0; i < elements.length; i++)
 {
-	var checkPageButton = document.getElementById('checkPage');
-	checkPageButton.addEventListener('click', function()
+	var element = elements[i];
+
+	for(var j = 0; j < element.childNodes.length; j++)
 	{
-		chrome.tabs.getSelected(null, function(tab)
-		{
-			d = document;
+		var node = element.childNodes[j];
 
-			var f = d.createElement('form');
-			f.action = 'http://gtmetrix.com/analyze.html?bm';
-			f.method = 'post';
+		if(node.nodeType === 3) {
+			var text = node.nodeValue;
+			var replacedText = text.replace(/Trump/gi, 'Jared');
 
-			var i = d.createElement('input');
-			i.type = 'hidden';
-			i.name = 'url';
-			i.value = tab.url;
-			f.appendChild(i);
-			d.body.appendChild(f);
-			f.submit();
-		});
-	}, false);
-}, false);
+			if(replacedText !== text) {
+				element.replaceChild(document.createTextNode(replacedText), node);
+			}
+		}
+	}
+}
