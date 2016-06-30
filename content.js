697270910bc58580447c6c110fd5a84831cb25ef
TempSpas <nicoverbeek@Belafonte.local>
2016-06-30 14:22:00 -0400

    Comment this out

--- content.js
+++ content.js
@@ -2,9 +2,9 @@ var context = document.querySelector("*");
 var instance = new Mark(context);
 instance.mark("Trump");
 
-$(document).ready(function() {
-	Tipped.create('instance', 'Some tooltip text');
-});
+// $(document).ready(function() {
+// 	Tipped.create('instance', 'Some tooltip text');
+// });
 
 // $(document).on('click', '#welcome_message:not(.active)', function(){
 //     $(this).addClass('active');
