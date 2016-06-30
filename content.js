034aba3654b20f253c5133f7612a6db010cb70e1
TempSpas <nicoverbeek@Belafonte.local>
2016-06-30 14:12:20 -0400

    Tooltip?

--- content.js
+++ content.js
@@ -1,3 +1,13 @@
 var context = document.querySelector("*"); 
 var instance = new Mark(context);
 instance.mark("Trump");
+
+$(document).ready(function() {
+	Tipped.create('mark', 'Some tooltip text');
+});
+
+// $(document).on('click', '#welcome_message:not(.active)', function(){
+//     $(this).addClass('active');
+//     $('.popup').show();
+//     return false;
+// })
