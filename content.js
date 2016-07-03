84376e336567a7708e416e9c348cac3d34e8e07a
TempSpas <nicoverbeek@Belafonte.home>
2016-07-03 11:31:05 -0400

    Initial commit
    This branch follows the advice given using a jQuery plugin from
    highlight.js instead of mark.js in order to highlight instances on a
    page.

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
-var context = document.querySelector("*"); 
-var instance = new Mark(context);
-instance.mark("Trump");
+// var context = document.querySelector("*"); 
+// var instance = new Mark(context);
+// instance.mark("Trump");
+
+$("body").highlight("Donald Trump");
+
 
 // $(document).ready(function() {
 // 	Tipped.create('instance', 'Some tooltip text');
