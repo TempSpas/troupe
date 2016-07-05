4c287270fa2dd74aac31929d67a792655a8a57e5
TempSpas <nicoverbeek@Belafonte.home>
2016-07-05 16:28:35 -0400

    Lay groundwork for the more advanced tooltip

--- content.js
+++ content.js
@@ -4,10 +4,22 @@
 
 $("body").highlight("Donald Trump");
 
+// This works!
+// $(document).ready(function() {
+// 	Tipped.create('.highlight', 'Some tooltip text');
+// });
 
 $(document).ready(function() {
-	Tipped.create('.highlight', 'Some tooltip text');
+	Tipped.create('.function', function(element) {
+		return {
+			title: $(element).data('title'),
+			content: $(element).data('content')
+		};
+	}, {
+		skin: 'light'
+	});
 });
+
 
 // $(document).ready(function() {
 // 	Tipped.create('instance', 'Some tooltip text');
