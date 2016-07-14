4f309771724de32a9a4997cce3aff5e28e7815d8
TempSpas <nicoverbeek@Belafonte.home>
2016-07-14 11:06:14 -0400

    Make some tweaks to help testing
    - Changes to highlight opacity (test)
    - Use George Clooney instead of Donald Trump so we can actually query
    tMDB
    - Tentative changes to the information in the manifest to better
    reflect the goal of the extension

--- content.js
+++ content.js
@@ -1,19 +1,15 @@
-// var context = document.querySelector("*"); 
-// var instance = new Mark(context);
-// instance.mark("Trump");
 
-$("body").highlight("Donald Trump");
-
-// This works!
-// $(document).ready(function() {
-// 	Tipped.create('.highlight', 'Some tooltip text');
-// });
+// Highlight all instances of the actor's name on the page
+$("body").highlight("George Clooney");
 
 // Need to implement the display of the tooltip, so we use this code
 $(document).ready(function() {
 	Tipped.create('.highlight', function(element)
 	{
 		var person = $('.highlight').html();
+		
+		var personToSearch = $(person).text($(person).html()); // Not sure if it will work
+
 		return {
 			title: person,
 			content: "content blah"
