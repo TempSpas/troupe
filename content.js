00ea28108d604dea771b0537517d13c8f105b622
TempSpas <nicoverbeek@Belafonte.local>
2016-07-14 15:13:10 -0400

    Try and pull ID

--- content.js
+++ content.js
@@ -2,9 +2,34 @@
 // Highlight all instances of the actor's name on the page
 $("body").highlight("George Clooney");
 
+var id;
+
 // Callback functions for responses
 function successCB(data) {
 	console.log("Success callback: " + data);
+
+	data = JSON.parse(data);
+	if(data.hasOwnProperty("results") && data.results.length > 0)
+	{
+        if(data.results.hasOwnProperty("id") && data.results.id.length > 0) {
+        	id = data.results.id;
+        	console.log(id);
+        }
+        // for (var i = data.results.length - 1; i >= 0; i--)
+        // {
+        //     if(data.results[i]["title"].length < 15 && data.results[i]["backdrop_path"] != "")
+        //     {
+        //     	var item = { title: "", img: ""};
+        //     	item.title = data.results[i]["title"].toUpperCase().split("");
+        //     	item.img = data.results[i]["backdrop_path"];
+        //     	this.movies.push(item);
+
+        //     }
+        // };
+        this.updateStage();
+    } else {
+        this.error("No Data");
+	}
 };
 
 function errorCB(data) {
@@ -18,7 +43,8 @@ $(document).ready(function() {
 		var person = $('.highlight').html();
 		//var personToSearch = $('person')[0].outerHTML; // Not sure if it will work
 
-		theMovieDb.search.getPerson({"query":"George%20Clooney"}, successCB, errorCB);
+		var json = theMovieDb.search.getPerson({"query":person}, successCB, errorCB);
+		// var obj = JSON.parse(json);
 
 		return {
 			title: person,
