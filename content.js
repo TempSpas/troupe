b8d69a6a6cc9bc931ca19c894008d90acc2ecf5e
TempSpas <nicoverbeek@Belafonte.home>
2016-07-15 10:47:25 -0400

    Attempt to parse the JSON inside the "main" function

--- content.js
+++ content.js
@@ -7,29 +7,6 @@ var id;
 // Callback functions for responses
 function successCB(data) {
 	console.log("Success callback: " + data);
-
-	data = JSON.parse(data);
-	if(data.hasOwnProperty("results") && data.results.length > 0)
-	{
-        if(data.results.hasOwnProperty("id") && data.results.id.length > 0) {
-        	id = data.results.id;
-        	console.log(id);
-        }
-        // for (var i = data.results.length - 1; i >= 0; i--)
-        // {
-        //     if(data.results[i]["title"].length < 15 && data.results[i]["backdrop_path"] != "")
-        //     {
-        //     	var item = { title: "", img: ""};
-        //     	item.title = data.results[i]["title"].toUpperCase().split("");
-        //     	item.img = data.results[i]["backdrop_path"];
-        //     	this.movies.push(item);
-
-        //     }
-        // };
-        // this.updateStage();
-    } else {
-        this.error("No Data");
-	}
 };
 
 function errorCB(data) {
@@ -41,10 +18,14 @@ $(document).ready(function() {
 	Tipped.create('.highlight', function(element)
 	{
 		var person = $('.highlight').html();
-		//var personToSearch = $('person')[0].outerHTML; // Not sure if it will work
-
-		var json = theMovieDb.search.getPerson({"query":person}, successCB, errorCB);
-		// var obj = JSON.parse(json);
+		
+		var json = theMovieDb.search.getPerson({"query":person}, function (data) {
+			data = JSON.parse(data);
+			if(data.hasOwnProperty("results") && data.results.length > 0) {
+				console.log("HELLO JOSE!");
+				// console.log("HELLO JOSE!" + data);
+			}
+		}, errorCB);
 
 		return {
 			title: person,
