7ad79e0ae0a93733ca5febaf192a8d13f9dc30b8
TempSpas <nicoverbeek@Belafonte.local>
2016-07-18 15:03:21 -0400

    Massive experiment

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-
+/**
 // Highlight all instances of the actor's name on the page
 $("body").highlight("George Clooney");
 
@@ -27,8 +27,6 @@ $(document).ready(function() {
 			if(data.hasOwnProperty("results") && data.results.length > 0) {
 				console.log("HELLO JOSE!");
 
-				alert(data);
-
 				for (var i = data.results.length - 1; i >= 0; i--) {
 					if(data.results[i]["name"].length < 15) {
 						// console.log("ID: ")
@@ -56,3 +54,57 @@ $(document).ready(function() {
 		skin: 'light'
 	});
 });
+**/
+
+
+// Highlight all instances of the actor's name on the page
+$("body").highlight("George Clooney");
+
+// var id;
+
+// Callback functions for responses
+function successCB(data) {
+	console.log("Success callback: " + data);
+};
+
+// Error callback function
+function errorCB(data) {
+	console.log("Error callback: " + data);
+	// https://www.themoviedb.org/documentation/api/status-codes
+};
+
+var person = $('.highlight').html();
+
+theMovieDb.search.getPerson({"query":person}, function (data) {
+	data = JSON.parse(data);
+	if(data.hasOwnProperty("results") && data.results.length > 0) {
+		console.log("HELLO JOSE!");
+
+		for (var i = data.results.length - 1; i >= 0; i--) {
+			if(data.results[i]["name"].length < 15) {
+				// console.log("ID: ")
+				var item = {name: "", id: "", img: ""};
+				item.name = data.results[i][name];
+				item.img = data.results[i]["profile_path"];
+				item.id = data.results[i]["id"];
+				id = item.id;
+
+				actors.push(item);
+				// console.log("Test ID: " + id);
+				// console.log("Test print id: " + item.id);
+				// console.log("Image: " + item.img);
+				$(document).ready(function() {
+					Tipped.create('.highlight', function(element)
+					{
+						return {
+							title: item.name,
+							content: item.id
+						};
+					}, {
+						skin: 'light'
+					});
+				});
+			}
+		}
+	}
+}.bind(this), errorCB);
