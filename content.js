ab856b3b62231470d7bd01bbf6791742b4417968
TempSpas <nicoverbeek@Belafonte.local>
2016-07-19 13:24:23 -0400

    Attempt to highlight multiple hardcoded names

--- content.js
+++ content.js
@@ -12,44 +12,89 @@ function errorCB(data) {
 	// https://www.themoviedb.org/documentation/api/status-codes
 };
 
-var person = $('.highlight').html();
+// var person = $('.highlight').html();
+
+// $( ".highlight" ).each(function( i ) {
+// 	var person = $(this).html();
+// }
 
 $(document).ready(function() {
-	theMovieDb.search.getPerson({"query":person}, function (data)
-	{
-		data = JSON.parse(data);
-		
-		if(data.hasOwnProperty("results") && data.results.length > 0)
+	$( ".highlight" ).each(function( i ) {
+		var person = $(this).html();
+
+		theMovieDb.search.getPerson({"query":person}, function (data)
 		{
-			console.log("HELLO JOSE!");
+			data = JSON.parse(data);
+			
+			if(data.hasOwnProperty("results") && data.results.length > 0)
+			{
+				console.log("HELLO JOSE!");
 
-			for (var i = data.results.length - 1; i >= 0; i--) {
-				if(data.results[i]["name"].length < 15 && data.results[i]["profile_path"] != "")
-				{
-					// console.log("ID: ")
-					var item = {name: "", id: "", img: ""};
-					item.name = data.results[i]["name"];
-					item.img = data.results[i]["profile_path"];
-					item.id = data.results[i]["id"];
-					id = item.id;
+				for (var i = data.results.length - 1; i >= 0; i--) {
+					if(data.results[i]["name"].length < 15 && data.results[i]["profile_path"] != "")
+					{
+						// console.log("ID: ")
+						var item = {name: "", id: "", img: ""};
+						item.name = data.results[i]["name"];
+						item.img = data.results[i]["profile_path"];
+						item.id = data.results[i]["id"];
+						id = item.id;
 
-					// console.log("Test ID: " + id);
-					// console.log("Test print id: " + item.id);
-					// console.log("Image: " + item.img);
-					$(document).ready(function() {
-						Tipped.create('.highlight', function(element)
-						{
-							return {
-								title: item.name,
-								content: item.id
-							};
-						}, {
-							skin: 'light'
+						// console.log("Test ID: " + id);
+						// console.log("Test print id: " + item.id);
+						// console.log("Image: " + item.img);
+						$(document).ready(function() {
+							Tipped.create('.highlight', function(element)
+							{
+								return {
+									title: item.name,
+									content: item.id
+								};
+							}, {
+								skin: 'light'
+							});
 						});
-					});
+					}
 				}
 			}
-		}
-	}.bind(this), errorCB);
+		}.bind(this), errorCB);		
+	}
+
+	// theMovieDb.search.getPerson({"query":person}, function (data)
+	// {
+	// 	data = JSON.parse(data);
+		
+	// 	if(data.hasOwnProperty("results") && data.results.length > 0)
+	// 	{
+	// 		console.log("HELLO JOSE!");
+
+	// 		for (var i = data.results.length - 1; i >= 0; i--) {
+	// 			if(data.results[i]["name"].length < 15 && data.results[i]["profile_path"] != "")
+	// 			{
+	// 				// console.log("ID: ")
+	// 				var item = {name: "", id: "", img: ""};
+	// 				item.name = data.results[i]["name"];
+	// 				item.img = data.results[i]["profile_path"];
+	// 				item.id = data.results[i]["id"];
+	// 				id = item.id;
+
+	// 				// console.log("Test ID: " + id);
+	// 				// console.log("Test print id: " + item.id);
+	// 				// console.log("Image: " + item.img);
+	// 				$(document).ready(function() {
+	// 					Tipped.create('.highlight', function(element)
+	// 					{
+	// 						return {
+	// 							title: item.name,
+	// 							content: item.id
+	// 						};
+	// 					}, {
+	// 						skin: 'light'
+	// 					});
+	// 				});
+	// 			}
+	// 		}
+	// 	}
+	// }.bind(this), errorCB);
 });
 
